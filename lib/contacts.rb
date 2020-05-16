9321597272e686567c26bb53e4550838dd162e59
Thanh Pham <tpham0123@gmail.com>
2020-05-16 22:56:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -28,5 +28,5 @@ def remove_strawberry(contacts)
      }
    }
   result = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
-  result.to_set ("strawberry")
+  result.to_change ("strawberry")
 end
