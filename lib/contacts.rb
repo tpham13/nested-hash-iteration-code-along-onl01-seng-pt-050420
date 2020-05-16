26395f148f3db64240645852dfd3f0139a7df440
Thanh Pham <tpham0123@gmail.com>
2020-05-16 22:55:17 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,17 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts = {
-    "Jon Snow" => {name: "Jon",email: "jon_snow@thewall.we", favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  contacts[:"John Snow"][:favorite_ice_cream_flavors]
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we", 
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
+  result = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
+  result.to_not include?("strawberry")
 end
